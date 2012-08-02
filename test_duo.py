f73f7bea3cf0f2f3a759e46e3b014e1322cf2449
deyk <deyk@crossway.org>
2012-08-02 06:07:28 -0700

    Added better comments around mocking out the boto interface.

--- test_duo.py
+++ test_duo.py
@@ -14,25 +14,35 @@ import mock
 
 
 class DynamoDBTests(unittest.TestCase):
+    # Default settings for describing the table we want to work with,
+    # in lieu of actual values from AWS.
+    default_item_data = dict(
+        table_name = 'test_table',
+        hash_key_name = 'test_hash_key',
+        range_key_name = 'test_range_key',
+        hash_key_value = 'fred',
+        range_key_value = 'flintstone',
+        item_attrs = {},
+        key = 'foo',
+        secret = 'bar',
+        )
+    
     def setUp(self):
-        self.table_name = 'test_table'
-        self.hash_key_name = 'test_hash_key'
-        self.range_key_name = 'test_range_key'
-        self.hash_key_value = 'fred'
-        self.range_key_value = 'flintstone'
-        self.item_attrs = {}
-        self.key = 'foo'
-        self.secret = 'bar'
-
+        super(DynamoDBTests, self).setUp()
+        for key, value in self.default_item_data.iteritems():
+            setattr(self, key, value)
+        
         from boto.dynamodb import layer1
         reload(layer1)
         self.boto_layer1 = layer1
+        # Mock out layer1 completely. This is where all the network interface occurs.
         MockLayer1 = mock.Mock(spec=layer1.Layer1)
         MockLayer1.return_value = MockLayer1
 
         layer1_patcher = self.layer1_patcher = mock.patch('boto.dynamodb.layer1.Layer1')
         self.mocked_layer1 = layer1_patcher.start()
 
+        # Spy on layer2, making sure that it gets a mock layer1 object.
         from boto.dynamodb import layer2
         reload(layer2)
         self.boto_layer2 = layer2
@@ -41,13 +51,17 @@ class DynamoDBTests(unittest.TestCase):
             wraps = layer2.Layer2())
         MockLayer2.layer1 = MockLayer1
 
+        # Mock out the connection to return our special MockLayer2.
         connect_patcher = self.connect_dynamodb_patcher = mock.patch('boto.connect_dynamodb')
         self.mocked_connect_dynamodb = connect_patcher.start()
         self.mocked_connect_dynamodb.return_value = MockLayer2
 
-        self.mock()
+        # Wire up
+        self.mock_boto_tables()
 
     def mock_item_data(self):
+        """Create a dict corresponding to an item description JSON from AWS.
+        """
         data = {
             "Item": {
                 self.hash_key_name: self.hash_key_value,
@@ -58,13 +72,16 @@ class DynamoDBTests(unittest.TestCase):
         data['Item'].update(self.item_attrs)
         return data
 
-    def mock(self):
+    def mock_boto_tables(self):
+        """Set up appropriate table-related stubs on the `boto.dynamodb.layer2` interface.
+        """
         from boto.dynamodb import table, item
         reload(table)
         reload(item)
         self.boto_table = table
         self.boto_item = item
 
+        # We want to be able to spy on the Table.
         MockTable = self.MockTable = mock.Mock(
             spec = table.Table,
             wraps = table.Table(self.MockLayer2, self.describe_table()))
@@ -78,11 +95,9 @@ class DynamoDBTests(unittest.TestCase):
         self.duo = duo
         self.db = duo.DynamoDB(key=self.key, secret=self.secret)
 
-    def tearDown(self):
-        self.connect_dynamodb_patcher.stop()
-        self.layer1_patcher.stop()
-
     def describe_table(self):
+        """Create a dict corresponding to a table description JSON from AWS.
+        """
         return {u'Table': {u'CreationDateTime': 1343759006.036,
                            u'ItemCount': 0,
                            u'KeySchema': {u'HashKeyElement': {u'AttributeName': self.hash_key_name,
@@ -95,6 +110,12 @@ class DynamoDBTests(unittest.TestCase):
                            u'TableSizeBytes': 0,
                            u'TableStatus': u'ACTIVE'}}
 
+    def tearDown(self):
+        self.connect_dynamodb_patcher.stop()
+        self.layer1_patcher.stop()
+
+
+class DuoTests(DynamoDBTests):
     def test_connection_on_db_should_be_lazily_created(self):
         """The DB connection should be lazily created when it's needed.
         """
