6d2fe6b6bc484f6e4599462f8d26a0011e7103d8
deyk <deyk@crossway.org>
2012-08-16 21:49:14 -0700

    Bumped to 0.2, for the first release.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ SETUP = dict(
         },
     zip_safe = False,
 
-    version = "0.1.2",
+    version = "0.2",
     description = "A powerful, dynamic, pythonic interface to AWS DynamoDB.",
     long_description = open(README).read(),
     author = "David Eyk",
