c9a839410fed2c67055d2977f52e0095095e2756
deyk <deyk@crossway.org>
2017-05-12 14:25:11 -0700

    Switch to moto for mocking dynamodb in tests

--- test_duo.py
+++ test_duo.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """tests -- Unit tests for duo.
 
-Mocking AWS services is HARD.
+Mocking AWS services is HARD. Moto is easy.
 """
 from __future__ import unicode_literals
 from six import with_metaclass, string_types, text_type, iteritems
-from six.moves import reload_module
 
 try:
     import unittest2 as unittest
@@ -14,7 +13,9 @@ except ImportError:
 
 import datetime
 
-import mock
+import boto
+import moto
+
 
 
 class DynamoDBTests(unittest.TestCase):
@@ -36,103 +37,30 @@ class DynamoDBTests(unittest.TestCase):
         for key, value in iteritems(self.default_item_data):
             setattr(self, key, value)
 
-        from boto.dynamodb import layer1
-        reload_module(layer1)
-        self.boto_layer1 = layer1
-        # Mock out layer1 completely. This is where all the network interface occurs.
-        MockLayer1 = mock.Mock(spec=layer1.Layer1)
-        MockLayer1.return_value = MockLayer1
-
-        layer1_patcher = self.layer1_patcher = mock.patch('boto.dynamodb.layer1.Layer1')
-        self.mocked_layer1 = layer1_patcher.start()
-
-        # Spy on layer2, making sure that it gets a mock layer1 object.
-        from boto.dynamodb import layer2
-        reload_module(layer2)
-        self.boto_layer2 = layer2
-        MockLayer2 = self.MockLayer2 = mock.Mock(
-            spec = layer2.Layer2,
-            wraps = layer2.Layer2())
-        MockLayer2.layer1 = MockLayer1
-
-        # Mock out the connection to return our special MockLayer2.
-        connect_patcher = self.connect_dynamodb_patcher = mock.patch('boto.connect_dynamodb')
-        self.mocked_connect_dynamodb = connect_patcher.start()
-        self.mocked_connect_dynamodb.return_value = MockLayer2
-
-        # Wire up
-        self.mock_boto_tables()
-
-    def mock_item_data(self):
-        """Create a dict corresponding to an item description JSON from AWS.
-        """
-        data = {
-            "Item": {
-                self.hash_key_name: self.hash_key_value,
-                self.range_key_name: self.range_key_value
-            },
-            "ConsumedCapacityUnits": 1
-        }
-        data['Item'].update(self.item_attrs)
-        return data
-
-    def mock_boto_tables(self):
-        """Set up appropriate table-related stubs on the `boto.dynamodb.layer2` interface.
-        """
-        from boto.dynamodb import table, item
-        reload_module(table)
-        reload_module(item)
-        self.boto_table = table
-        self.boto_item = item
-
-        # We want to be able to spy on the Table.
-        MockTable = self.MockTable = mock.Mock(
-            spec = table.Table,
-            wraps = table.Table(self.MockLayer2, self.describe_table()))
-
-        self.MockLayer2.describe_table.return_value = self.describe_table()
-        self.MockLayer2.get_table.return_value = MockTable
-        self.MockLayer2.layer1.get_item.return_value = self.mock_item_data()
+        self.dynamo_patcher = moto.mock_dynamodb()
+        self.dynamo_patcher.start()
 
         import duo
-        reload_module(duo)
         self.duo = duo
         self.db = duo.DynamoDB(key=self.key, secret=self.secret)
-
-    def describe_table(self):
-        """Create a dict corresponding to a table description JSON from AWS.
-        """
-        return {'Table': {'CreationDateTime': 1343759006.036,
-                          'ItemCount': 0,
-                          'KeySchema': {'HashKeyElement': {'AttributeName': self.hash_key_name,
-                                                           'AttributeType': 'S'},
-                                        'RangeKeyElement': {'AttributeName': self.range_key_name,
-                                                            'AttributeType': 'S'}},
-                          'ProvisionedThroughput': {'ReadCapacityUnits': 10,
-                                                    'WriteCapacityUnits': 5},
-                          'TableName': self.table_name,
-                          'TableSizeBytes': 0,
-                          'TableStatus': 'ACTIVE'}}
+        self.schema = self.db.connection.create_schema(
+            hash_key_name=self.hash_key_name,
+            hash_key_proto_value=str,
+            range_key_name=self.range_key_name,
+            range_key_proto_value=str,
+        )
+        self.db.connection.create_table(self.table_name, self.schema, 10, 10)
 
     def tearDown(self):
-        self.connect_dynamodb_patcher.stop()
-        self.layer1_patcher.stop()
+        self.dynamo_patcher.stop()
 
 
 class DuoTests(DynamoDBTests):
-    def test_connection_on_db_should_be_lazily_created(self):
-        """The DB connection should be lazily created when it's needed.
-        """
-        self.assertEqual(self.mocked_connect_dynamodb.call_count, 0)
-        self.db.connection
-        self.assertEqual(self.mocked_connect_dynamodb.call_count, 1)
-        self.assertTrue(self.mocked_connect_dynamodb.called_with_arguments(self.key, self.secret))
-
     def test_getitem_on_db_should_return_table_of_given_name(self):
         """duo.DynamoDB()[name] should return a table of the given name.
         """
         table = self.db[self.table_name]
-        self.assertIs(table.table, self.MockTable)
+        self.assertIsInstance(table.table, boto.dynamodb.table.Table)
         self.assertEqual(table.table_name, self.table_name)
 
     def test_getitem_on_table_should_return_item(self):
@@ -141,7 +69,7 @@ class DuoTests(DynamoDBTests):
         table = self.db[self.table_name]
         item = table[self.hash_key_value, self.range_key_value]
 
-        self.assertIsInstance(item, self.boto_item.Item)
+        self.assertIsInstance(item, boto.dynamodb.item.Item)
         self.assertIsInstance(item, self.duo.Item)
         self.assertEqual(item[self.hash_key_name], self.hash_key_value)
 
